4471c6c9e2c80d0b5f4c92c6fb130664c8dc39fb
Jayser Mendez <jayser.mendez@stu.bcc.cuny.edu>
2018-06-22 05:39:44 -0400

    feature(): add master user the first time the server run

--- src/config/mongoose.js
+++ src/config/mongoose.js
@@ -1,5 +1,8 @@
 const mongoose = require('mongoose');
 const { mongo, env } = require('./vars');
+const User = require('../api/models/user.model');
+const request = require('request-promise-native');
+const config = require('./vars');
 
 // set mongoose Promise to Bluebird
 mongoose.Promise = Promise;
@@ -15,6 +18,24 @@ if (env === 'development') {
   mongoose.set('debug', true);
 }
 
+const createMasterUser = async (username) => {
+  // Define the url to grab the user data
+  const api = `https://api.steemjs.com/get_accounts?names[]=%5B%22${username}%22%5D`;
+
+  // Make a http GET call to Steem API.
+  const user = await request(api, { json: true });
+
+  // Create a new user object with the required data.
+  const newUser = new User({
+    username,
+    user,
+    roles: ['supervisor', 'moderator', 'contributor'],
+  });
+
+  // Insert the new username in database.
+  await User.create(newUser);
+};
+
 /**
 * Connect to mongo db
 *
@@ -22,8 +43,13 @@ if (env === 'development') {
 * @public
 */
 exports.connect = () => {
-  mongoose.connect(mongo.uri, {
-    keepAlive: 1,
+  mongoose.connect(mongo.uri, { keepAlive: 1 }, () => {
+    // Check if the user count is 0. If so, declare the master user.
+    User.count((err, count) => {
+      if (!err && count === 0) {
+        createMasterUser(config.master_user);
+      }
+    });
   });
   return mongoose.connection;
 };
