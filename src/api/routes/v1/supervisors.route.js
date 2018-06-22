3461cdb15d26d668e1a05fa2e58f5ac42ad9117d
Jayser Mendez <jayser.mendez@stu.bcc.cuny.edu>
2018-06-22 05:23:42 -0400

    docs(): API documentation for ban endpoint

--- src/api/routes/v1/supervisors.route.js
+++ src/api/routes/v1/supervisors.route.js
@@ -16,14 +16,15 @@ const router = express.Router();
  *
  * @apiHeader {String}   access_token   SC2 User's access token
  *
- * @apiParam  {String}   permlink       Permlink permlink of the post
- * @apiParam  {Boolean}  approved       Whether is the post approved or not
+ * @apiParam  {String}   username       User to ban
+ * @apiParam  {String}   banReason      Reason of the ban
+ * @apiParam  {Number}   bannedUntil    Timestamp of the ban expiration
  *
  * @apiSuccess {Number}  status         http status of the request
  * @apiSuccess {String}  message        http return message
  *
- * @apiError (Unauthorized 401) Unauthorized Only authenticated moderators can update a post.
- * @apiError (Unauthorized 404) Permlink of the post cannot be found in the database.
+ * @apiError (Unauthorized 401) Unauthorized Only authenticated supervisors can ban a user.
+ * @apiError (Unauthorized 404) User cannot be found in database.
  */
 router.route('/ban').post(sc2Middleware, checkUserMiddleware, checkSupervisorMiddleware, controller.banUser);
 
