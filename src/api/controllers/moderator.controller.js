d1693155c3bdb0a8bc40baca37620f6d45d9b185
Jayser Mendez <jayser.mendez@stu.bcc.cuny.edu>
2018-06-22 06:14:11 -0400

    feature(): prevent moderators to moderate a post more than once

--- src/api/controllers/moderator.controller.js
+++ src/api/controllers/moderator.controller.js
@@ -1,4 +1,5 @@
 const Post = require('../models/post.model');
+const ModeratedPost = require('../models/moderated.model');
 const httpStatus = require('http-status');
 
 /**
@@ -14,6 +15,17 @@ exports.moderatePost = async (req, res, next) => {
     // Grab the moderator username from the locals
     const moderator = res.locals.username;
 
+    // Ask database if this moderator has moderated this post before
+    const isModerated = await ModeratedPost.findOne({ permlink, moderatedBy: moderator });
+
+    // If this post is already moderated, prevent the moderator to moderate it again.
+    if (isModerated) {
+      return res.send({
+        status: httpStatus.UNAUTHORIZED,
+        message: 'You\'ve already moderated this post. Contact a supervisor to change its status.',
+      });
+    }
+
     // Update the post with the new moderation data
     const post = await Post.findOneAndUpdate(
       { permlink },
@@ -21,12 +33,25 @@ exports.moderatePost = async (req, res, next) => {
         'moderation.moderated': true,
         'moderation.approved': approved,
         'moderation.moderatedBy': moderator,
-        'moderation.moderatedAt': new Date(),
+        'moderation.moderatedAt': +new Date(),
       },
     );
 
+    // Create a new moderated post with the data above.
+    const moderation = new ModeratedPost({
+      moderated: true,
+      approved,
+      moderatedBy: moderator,
+      moderatedAt: +new Date(),
+      permlink,
+      category: post.category,
+    });
+
+    // Insert the new moderated post into its respective collection.
+    const moderatedPost = await ModeratedPost.create(moderation);
+
     // If the post is moderated correctly, send the message to the client.
-    if (post) {
+    if (post && moderatedPost) {
       return res.send({
         status: 200,
         message: 'Post moderated correctly',
---
+++ src/api/models/moderated.model.js
@@ -0,0 +1,52 @@
+const mongoose = require('mongoose');
+
+/**
+ * Moderated Posts Schema
+ * @author Jayser Mendez
+ * @private
+ */
+const moderatedSchema = new mongoose.Schema({
+  moderated: {
+    type: Boolean,
+    default: false,
+  },
+  approved: {
+    type: Boolean,
+    default: false,
+  },
+  moderatedBy: {
+    type: String,
+    required: true,
+    trim: true,
+    lowercase: true,
+  },
+  moderatedAt: {
+    type: Date,
+    default: null,
+  },
+  permlink: {
+    type: String,
+    required: true,
+    trim: true,
+    lowercase: true,
+  },
+  category: {
+    type: String,
+    required: true,
+    trim: true,
+    lowercase: true,
+  },
+}, {
+  timestamps: true,
+});
+
+// Declare index for createdAt property.
+moderatedSchema.index({ createdAt: -1 });
+
+// Declare index for author and category.
+moderatedSchema.index({ moderatedBy: 1, approved: 1, permlink: 1 });
+
+/**
+ * @typedef Post
+ */
+module.exports = mongoose.model('ModeratedPost', moderatedSchema);
