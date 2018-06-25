9a77b8ac0a8de216572ec1286b1f4b5e73ac9bd7
Jayser Mendez <jayser.mendez@stu.bcc.cuny.edu>
2018-06-25 15:08:30 -0400

    fix(): middleware to check roles

--- src/api/middlewares/check_role.js
+++ src/api/middlewares/check_role.js
@@ -7,40 +7,43 @@ const httpStatus = require('http-status');
  * @author Jayser Mendez.
  * @public
  */
-const checkRole = async (role, req, res, next) => {
-  try {
-    // Try to find the username in database.
-    const user = await User.findOne({ username: res.locals.username });
+// eslint-disable-next-line
+const checkRole = (role) => {
+  return async (req, res, next) => {
+    try {
+      // Try to find the username in database.
+      const user = await User.findOne({ username: res.locals.username });
 
-    // If the user is found, check if the user indicated role.
-    if (user) {
-      if (user.roles.indexOf(role) > -1) {
-        // Pass to the next middleware if the indicated role is present in this user.
-        return next();
+      // If the user is found, check if the user indicated role.
+      if (user) {
+        if (user.roles.indexOf(role) > -1) {
+          // Pass to the next middleware if the indicated role is present in this user.
+          return next();
+        }
+
+        // If the user has not the indicated role, return an error in the middleware
+        return next({
+          status: httpStatus.UNAUTHORIZED,
+          message: 'Unauthorized access',
+        });
       }
 
-      // If the user has not the indicated role, return an error in the middleware
+      // If not user is found, let the client know that this user does not exist
       return next({
         status: httpStatus.UNAUTHORIZED,
-        message: 'Unauthorized access',
+        message: 'This username does not exist in our records. Unauthorized access.',
+      });
+
+      // Catch any possible error.
+    } catch (err) {
+      // Catch errors here.
+      return next({
+        status: httpStatus.INTERNAL_SERVER_ERROR,
+        message: 'Opps! Something is wrong in our server. Please report it to the administrator.',
+        error: err,
       });
     }
-
-    // If not user is found, let the client know that this user does not exist
-    return next({
-      status: httpStatus.UNAUTHORIZED,
-      message: 'This username does not exist in our records. Unauthorized access.',
-    });
-
-  // Catch any possible error.
-  } catch (err) {
-    // Catch errors here.
-    return next({
-      status: httpStatus.INTERNAL_SERVER_ERROR,
-      message: 'Opps! Something is wrong in our server. Please report it to the administrator.',
-      error: err,
-    });
-  }
+  };
 };
 
 module.exports = checkRole;
