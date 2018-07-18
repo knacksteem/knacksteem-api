537f961be97fcad7d0eb1fc4829623243edceb99
Jayser Mendez <jayser.mendez@stu.bcc.cuny.edu>
2018-07-18 15:09:58 -0400

    fix(): Allow all method for CORS in dev
    Allow CORS Put method to fix the following error:
    
    "Method PUT is not allowed by Access-Control-Allow-Methods in preflight response"

--- src/config/express.js
+++ src/config/express.js
@@ -63,6 +63,7 @@ if (env !== 'development') {
   app.use((req, res, next) => {
     res.header('Access-Control-Allow-Origin', '*');
     res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+    res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
     next();
   });
 }
