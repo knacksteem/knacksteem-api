7828c2e7b267766702b5fc85f99b57f12960b1af
Mohit Sharma <msvbs98@gmail.com>
2018-07-30 12:13:19 +0530

    Make some changes

--- src/index.js
+++ src/index.js
@@ -1,12 +1,25 @@
 const { port, env } = require('./config/vars');
 const app = require('./config/express');
 const mongoose = require('./config/mongoose');
+const https = require('https');
+const fs = require('fs');
 
 // open mongoose connection
 mongoose.connect();
+let options;
+if (env === 'production') {
+  options = {
+    key: fs.readFileSync('./privkey.pem'),
+    cert: fs.readFileSync('./fullchain.pem'),
+  };
+}
 
 // listen to requests
-app.listen(port, () => console.info(`server started on port ${port} (${env})`));
+if (env === 'production') {
+  https.createServer(options, app).listen(port);
+} else {
+  app.listen(port, () => console.info(`server started on port ${port} (${env})`));
+}
 
 /**
 * Exports express
