1fb59f5483bd173152a1c68bf10fcb0dca06a8eb
tdreid <trevor.d.reid@gmail.com>
2018-08-05 16:47:48 +0000

    Remove modular checkForEligiblePost.js

--- bot/bot.js
+++ bot/bot.js
@@ -14,7 +14,6 @@ cron.scheduleJob(process.env.KNACKBOT_SCHEDULE, () => {
   getVotingPower(process.env.KNACKBOT_USER, vp => {
     if (vp >= (Number(process.env.KNACKBOT_VOTING_THRESHOLD) || 99.98)) {
       logger.info(`Voting power of ${vp} meets threshold.`);
-      checkForEligiblePost;
     } else {
       logger.info(`Voting power of ${vp} is below threshold.`);
     }
