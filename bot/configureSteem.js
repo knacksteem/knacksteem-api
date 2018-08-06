b4745ede5b09707c6cfd0621d178591d0a3c9a95
tdreid <trevor.d.reid@gmail.com>
2018-08-06 01:06:31 +0000

    Return configured steem api object

--- bot/configureSteem.js
+++ bot/configureSteem.js
@@ -1,25 +1,24 @@
-const steem = require('steem');
 const logger = require('./configureLogger');
+const steem = require('steem');
 
-logger.debug(process.env);
-
-if (process.env.KNACKBOT_MODE == 'TEST') {
-  const settings = {
-    url: process.env.KNACKBOT_STEEM_TEST_URL || 'wss://testnet.steem.vc',
-    address_prefix: process.env.KNACKBOT_STEEM_TEST_PREFIX || 'STX',
-    chain_id:
-      process.env.KNACKBOT_STEEM_TEST_CHAIN_ID ||
-      '79276aea5d4877d9a25892eaa01b0adf019d3e5cb12a97478df3298ccdd01673'
-  };
-  steem.api.setOptions(settings);
-  logger.info(
-    `Steem url set to test. Settings: \n${JSON.stringify(settings, null, 2)}`
-  );
-} else {
-  steem.api.setOptions({
-    url: process.env.KNACKBOT_STEEM_URL || 'https://api.steemit.com'
-  });
-  logger.info('Steem url set to live.');
-}
-
-module.exports = steem;
+module.exports = () => {
+  if (process.env.KNACKBOT_MODE == 'TEST') {
+    const settings = {
+      url: process.env.KNACKBOT_STEEM_TEST_URL || 'wss://testnet.steem.vc',
+      address_prefix: process.env.KNACKBOT_STEEM_TEST_PREFIX || 'STX',
+      chain_id:
+        process.env.KNACKBOT_STEEM_TEST_CHAIN_ID ||
+        '79276aea5d4877d9a25892eaa01b0adf019d3e5cb12a97478df3298ccdd01673'
+    };
+    steem.api.setOptions(settings);
+    logger.info(
+      `Steem url set to test. Settings: \n${JSON.stringify(settings, null, 2)}`
+    );
+  } else {
+    steem.api.setOptions({
+      url: process.env.KNACKBOT_STEEM_URL || 'https://api.steemit.com'
+    });
+    logger.info('Steem url set to live.');
+  }
+  return steem;
+};
