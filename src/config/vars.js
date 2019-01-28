629847296743eb75a22377c8a2f0faedc8986b9b
Jayser Mendez <jayser.mendez@stu.bcc.cuny.edu>
2019-01-27 21:12:24 -0800

    replaced SC2 app name

--- src/config/vars.js
+++ src/config/vars.js
@@ -13,7 +13,7 @@ module.exports = {
   steemApi: process.env.STEEM_API || 'https://api.steemjs.com',
   botAccount: process.env.BOT_ACCOUNT || 'knacksteem.org',
   botKey: process.env.BOT_KEY,
-  sc2_app: process.env.SC2_APP || 'steemia.app',
+  sc2_app: process.env.SC2_APP || 'knacksteem.app',
   sc2_callback: process.env.SC2_CALLBACK || 'http://localhost:3030',
   master_user: process.env.MASTER_USER || 'knowledges',
   env: process.env.NODE_ENV,
